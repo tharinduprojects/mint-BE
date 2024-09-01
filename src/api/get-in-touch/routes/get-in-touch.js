2df955e40c592bf84f297f609d04cfc9d9cf8331
mustythemist <mustythemist@gmail.com>
2024-09-01 22:43:04 +0530

    get in touch schema added

---
+++ src/api/get-in-touch/routes/get-in-touch.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * get-in-touch router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::get-in-touch.get-in-touch');
